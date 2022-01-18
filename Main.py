77c2645f1c52cd6d569958671cfd4667bd4c78a3
Gwendal <gwendou95@hotmail.fr>
2022-01-18 22:18:09 +0100

    ÃActivate all questions

--- Main.py
+++ Main.py
@@ -19,48 +19,49 @@ df = spark.read.format("csv") \
     .load(dataset_path)
 print("Loading OK !")
 
-# print("Dataset head")
-# df.show(5)
+# Init start time
+start_time = datetime.now()
 
 # Question 1
-# print("Question 1")
+print("--- Question 1")
 
 # group by repo and count commits
-# df.groupBy("repo")\
-#     .count()\
-#     .orderBy("count", ascending=False) \
-#     .show(10, truncate=False)
+df.groupBy("repo")\
+    .count()\
+    .orderBy("count", ascending=False) \
+    .show(10, truncate=False)
 
 # Question 2
-# print("Question 2")
+print("--- Question 2")
 
-# df.filter(col("repo") == "apache/spark") \
-#     .groupBy("author") \
-#     .count() \
-#     .orderBy("count", ascending=False) \
-#     .show(1, truncate=False)
+df.filter(col("repo") == "apache/spark") \
+    .groupBy("author") \
+    .count() \
+    .orderBy("count", ascending=False) \
+    .show(1, truncate=False)
 
 # Question 3
-# print("Question 3")
+print("--- Question 3")
 # filter by repo
 # cast date column to timestamp
-# spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
+spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
 
 # date from 6 months ago, but we do not have enough data
-# date_six_months_ago = int((datetime.now() - timedelta(days=183)).timestamp())
+date_six_months_ago = int((datetime.now() - timedelta(days=183)).timestamp())
 # So we take a date from 8 years and 6 months ago
-# date_eight_year_and_six_months_ago = int((datetime.now() - timedelta(days=(183 + 365 * 8))).timestamp())
+date_eight_year_and_six_months_ago = int((datetime.now() - timedelta(days=(183 + 365 * 8))).timestamp())
 
 # We make our calcul on the timestamp to be more performant
-# df.filter((col("repo") == "apache/spark")) \
-#         .withColumn("timestamp", unix_timestamp(df.date, 'E MMM d HH:mm:ss yyyy Z')) \
-#         .where(col("timestamp").between(date_eight_year_and_six_months_ago, int(datetime.now().timestamp()))) \
-#         .groupBy("author") \
-#         .count() \
-#         .orderBy("count", ascending=False) \
-#         .show(5, truncate=False)
+df.filter((col("repo") == "apache/spark")) \
+        .withColumn("timestamp", unix_timestamp(df.date, 'E MMM d HH:mm:ss yyyy Z')) \
+        .where(col("timestamp").between(date_eight_year_and_six_months_ago, int(datetime.now().timestamp()))) \
+        .groupBy("author") \
+        .count() \
+        .orderBy("count", ascending=False) \
+        .show(5, truncate=False)
 
 # Question 4
+print("--- Question 4")
 # load stop words list
 stop_words_path_txt = "data/englishST.txt"
 stop_words = spark.sparkContext.textFile(stop_words_path_txt).collect()
@@ -79,3 +80,6 @@ df.withColumn("words", split(df.message, " ")) \
     .show(10, truncate=False)
 
 # .subtract(stop_words) \
+# Print total time
+print("--- Total time : " + str(datetime.now() - start_time) + " seconds")
+
